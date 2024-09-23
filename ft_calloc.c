59a2d476314dd0a055827abc3dea45976ab8fdac
hoskim <hoskim@student.42prague.com>
2024-09-23 13:20:54 +0200

    commit

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 22:09:51 by hoskim            #+#    #+#             */
-/*   Updated: 2024/09/21 13:53:18 by hoskim           ###   ########.fr       */
+/*   Created: 2024/09/21 21:30:20 by hoskim            #+#    #+#             */
+/*   Updated: 2024/09/21 21:30:20 by hoskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
 
-void *memset(void *pointer, int value, size_t number) {
-    
-    char *p;
-    size_t i;
+void *ft_calloc(size_t num, size_t size)
+{
+    char *ptr;
 
-    p = pointer;
-    i = 0;
-
-    while(i < number)
-        p[i++] = value;
-    return(pointer);
+    ptr = malloc(num * size);
+    if (!(ptr))
+    {
+        return (NULL);
+    }
+    ft_memset(ptr, 0, num * size);
+    return (ptr);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -1,26 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 22:09:51 by hoskim            #+#    #+#             */
-/*   Updated: 2024/09/21 13:53:18 by hoskim           ###   ########.fr       */
+/*   Created: 2024/09/22 19:14:55 by hoskim            #+#    #+#             */
+/*   Updated: 2024/09/22 19:14:55 by hoskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *memset(void *pointer, int value, size_t number) {
-    
-    char *p;
-    size_t i;
+char *strdup(const char *str)
+{
+    char *dup;
+    int i;
+    int strlen;
 
-    p = pointer;
+    if (!str)
+    {
+        return (NULL);
+    }
+
+    strlen = (int)ft_strlen(str);
+    dup = (char *)malloc(strlen + 1);
+
+    if (!(dup))
+    {
+        return (NULL);
+    }
+
     i = 0;
-
-    while(i < number)
-        p[i++] = value;
-    return(pointer);
+    
+    while (str[i])
+    {
+        dup[i] = str[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return(dup);
 }
